08a66dda0b69fcf61ae24c8052a6e293be080262
hasanshaih <hafizov.marat@mail.ru>
2021-06-04 20:46:30 +0300

    Initial commit

---
+++ src/com/test/Main.java
@@ -0,0 +1,15 @@
+package com.test;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
+/*
+ПОРЯДОК ВЫПОЛНЕНИЯ:
+1. static блоки
+2. потом то, что в методе main
+3. если создается объект класса, тогда init блоки
+4. потом конструктор
+*/
